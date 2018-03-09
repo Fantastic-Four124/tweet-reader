644a4dd2613d4c536e6b16df3da099f716cbeef9
vagrant <vagrant@precise64.(none)>
2018-03-09 03:57:37 +0000

    Testing GitHub webhooks

--- service.rb
+++ service.rb
@@ -15,20 +15,24 @@ set :bind, '0.0.0.0' # Needed to work with Vagrant
 
 get '/api/v1/tweets/:user_id' do # Get tweets by :user_id
   #tweets = Tweet.where(user_id: params[:user_id]).desc(:date_posted).limit(50).to_h
+  #redirect '/api/v1/{route}'
 end
 
 get '/api/v1/tweets/recent' do # Get 50 random tweets
   #tweets = Tweet.desc(:date_posted).limit(50).to_h
+  #redirect '/api/v1/{route}'
 end
 
 get '/api/v1/tweets/:tweet_id/hashtags' do # Get hashtags associated with the tweet
   #hashtags = Array.new
   #tweets = Tweet.find_by(params[:tweet_id]).each do |tweet| #Should only execute once
     #hastags = tweet.hashtags
+  #redirect '/api/v1/{route}'
 end
 
 get '/api/v1/tweets/:tweet_id/mentions' do # Get mention associated with the tweet
   #mentions = Array.new
   #tweets = Tweet.find_by(params[:tweet_id]).each do |tweet| #Should only execute once
     #hastags = tweet.mentions
+  #redirect '/api/v1/{route}'
 end
