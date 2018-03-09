8244fbf97b9c6dcd90ea0445650bee90a4a5142e
vagrant <vagrant@precise64.(none)>
2018-03-09 03:31:49 +0000

    Skeleton established

---
+++ README.md
@@ -0,0 +1,11 @@
+#tweet-reader
+
+##A prototype for MongoDB services in Ruby and Sinatra
+___
+###Basic
+
+This is the bare-bones assembly for a service using the MongoDB, utilizing the *mongoid* gem and the resource *mLab*.
+
+The intent is for this to become the reading service for Tweets for the accompanying nanoTwitter application. As such, it is designed to retrieve Tweets.
+
+It will probably need to have the Redis cache, or whatever we need to handle reads more effectively.
---
+++ service.rb
@@ -0,0 +1,34 @@
+# server.rb
+require 'sinatra'
+require 'mongoid'
+require_relative 'models/tweet'
+
+# DB Setup
+Mongoid.load! "mongoid.config"
+
+#set binding
+enable :sessions
+
+set :bind, '0.0.0.0' # Needed to work with Vagrant
+
+# These are still under construction.
+
+get '/api/v1/tweets/:user_id' do # Get tweets by :user_id
+  #tweets = Tweet.where(user_id: params[:user_id]).desc(:date_posted).limit(50).to_h
+end
+
+get '/api/v1/tweets/recent' do # Get 50 random tweets
+  #tweets = Tweet.desc(:date_posted).limit(50).to_h
+end
+
+get '/api/v1/tweets/:tweet_id/hashtags' do # Get hashtags associated with the tweet
+  #hashtags = Array.new
+  #tweets = Tweet.find_by(params[:tweet_id]).each do |tweet| #Should only execute once
+    #hastags = tweet.hashtags
+end
+
+get '/api/v1/tweets/:tweet_id/mentions' do # Get mention associated with the tweet
+  #mentions = Array.new
+  #tweets = Tweet.find_by(params[:tweet_id]).each do |tweet| #Should only execute once
+    #hastags = tweet.mentions
+end
