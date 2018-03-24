157f4ccff3f5f913d54f71fff23cd323c033d4c6
vagrant <vagrant@precise64.(none)>
2018-03-24 20:56:03 +0000

    basic read functionality established

--- service.rb
+++ service.rb
@@ -1,6 +1,8 @@
 # server.rb
 require 'sinatra'
 require 'mongoid'
+require 'json'
+require 'byebug'
 require_relative 'models/tweet'
 
 # DB Setup
@@ -10,29 +12,22 @@ Mongoid.load! "mongoid.config"
 enable :sessions
 
 set :bind, '0.0.0.0' # Needed to work with Vagrant
-
+set :port, 8090
 # These are still under construction.
 
 get '/api/v1/tweets/:user_id' do # Get tweets by :user_id
-  #tweets = Tweet.where(user_id: params[:user_id]).desc(:date_posted).limit(50).to_h
-  #redirect '/api/v1/{route}'
+  #byebug
+  tweets = Tweet.where(user_id: params[:id]).desc(:date_posted).limit(50).to_json
 end
 
 get '/api/v1/tweets/recent' do # Get 50 random tweets
-  #tweets = Tweet.desc(:date_posted).limit(50).to_h
-  #redirect '/api/v1/{route}'
+  tweets = Tweet.desc(:date_posted).limit(50).to_json
 end
 
 get '/api/v1/tweets/:tweet_id/hashtags' do # Get hashtags associated with the tweet
-  #hashtags = Array.new
-  #tweets = Tweet.find_by(params[:tweet_id]).each do |tweet| #Should only execute once
-    #hastags = tweet.hashtags
-  #redirect '/api/v1/{route}'
+  Tweet.find_by(params[:tweet_id]).hashtags.to_json
 end
 
 get '/api/v1/tweets/:tweet_id/mentions' do # Get mention associated with the tweet
-  #mentions = Array.new
-  #tweets = Tweet.find_by(params[:tweet_id]).each do |tweet| #Should only execute once
-    #hastags = tweet.mentions
-  #redirect '/api/v1/{route}'
+  Tweet.find_by(params[:tweet_id]).mentions.to_json
 end
