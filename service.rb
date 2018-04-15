f8b25982cf14080103289fb8dcc3211de7a41e56
vagrant <vagrant@precise64.(none)>
2018-04-15 21:03:33 +0000

    started on Redis implementation

--- service.rb
+++ service.rb
@@ -23,60 +23,95 @@ set :allow_headers, 'accept,content-type,if-modified-since'
 set :expose_headers, 'location,link'
 
 configure do
-  uri = URI.parse("redis-19695.c8.us-east-1-3.ec2.cloud.redislabs.com:19695")
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  user_uri = URI.parse(ENV["USER_REDIS_URL"])
+  follow_uri = URI.parse(ENV["FOLLOW_REDIS_URL"])
+  reader_uri = URI.parse(ENV["READER_REDIS_URL"])
+  $user_redis = Redis.new(:host => user_uri.host, :port => user_uri.port, :password => user_uri.password)
+  $follow_redis = Redis.new(:host => follow_uri.host, :port => follow_uri.port, :password => follow_uri.password)
+  $reader_redis = Redis.new(:host => reader_uri.host, :port => reader_uri.port, :password => reader_uri.password)
+
+end
+
+helpers do
+  def authorized? token
+    return !$user_redis[token].nil?
 end
 
 get '/loaderio-e30c4c1f459b4ac680a9e6cc226a3199.txt' do
   send_file 'loaderio-e30c4c1f459b4ac680a9e6cc226a3199.txt'
 end
 
-get '/api/v1/tweets/:username' do # Get tweets by :username
+get '/api/v1/:apitoken/tweets/:username' do # Get tweets by :username
   #byebug
-  tweets = Tweet.where(username: params[:name]).desc(:date_posted).limit(50).to_json
-  # Choo implementation
-  # tweetJSON = Array.new
-  # tweets.each do |tweet|
-  #   tweetJSON <<  {
-  #     contents: tweet.contents,
-  #     createdAt: tweet.date_posted,
-  #     id: tweet.id,
-  #     user: {
-  #       username: tweet.username,
-  #       id: tweet.user_id
-  #     }
-  #   }
-  # end
-  # tweetJSON.to_json
+  if authorized? params[:apitoken]
+    # if $reader_redis.get(params[:user_id] + "_feed").nil?
+    tweets = Tweet.where(username: params[:name]).desc(:date_posted).limit(50).to_json
+    # Choo implementation
+    # tweetJSON = Array.new
+    # tweets.each do |tweet|
+    #   tweetJSON <<  {
+    #     contents: tweet.contents,
+    #     createdAt: tweet.date_posted,
+    #     id: tweet.id,
+    #     user: {
+    #       username: tweet.username,
+    #       id: tweet.user_id
+    #     }
+    #   }
+    # $reader_redis.lpush(params[:user_id] + "_feed", tweetJSON)
+    # end
+    # tweetJSON.to_json
+    # else $reader_redis.get(params[:user_id] + "_feed").to_json
+  else
+    {err: true}.to_json
+  end
 end
 
-get '/api/v1/tweets/:tweet_id' do
-  Tweet.find_by(params[:tweet_id]).to_json
+get '/api/v1/:apitoken/tweets/:tweet_id' do
+  if authorized? params[:apitoken]
+    Tweet.find_by(params[:tweet_id]).to_json
+  else
+    {err: true}.to_json
+  end
 end
 
-get '/api/v1/tweets/recent' do # Get 50 random tweets
-  tweets = Tweet.desc(:date_posted).limit(50).to_json
-  # Choo implementation
-  # tweetJSON = Array.new
-  # tweets.each do |tweet|
-  #   tweetJSON <<  {
-  #     contents: tweet.contents,
-  #     createdAt: tweet.date_posted,
-  #     id: tweet.id,
-  #     user: {
-  #       username: tweet.username,
-  #       id: tweet.user_id
-  #     }
-  #   }
-  # end
-  # tweetJSON.to_json
+get '/api/v1/:apitoken/tweets/recent' do # Get 50 random tweets
+  if authorized? params[:apitoken]
+    # if $reader_redis.get("recent").nil?
+    tweets = Tweet.desc(:date_posted).limit(50).to_json
+    # Choo implementation
+    # tweetJSON = Array.new
+    # tweets.each do |tweet|
+    #   tweetJSON <<  {
+    #     contents: tweet.contents,
+    #     createdAt: tweet.date_posted,
+    #     id: tweet.id,
+    #     user: {
+    #       username: tweet.username,
+    #       id: tweet.user_id
+    #     }
+    #   }
+    # $reader_redis.lpush(tweetJSON)
+    # end
+    # tweetJSON.to_json
+    # else return $reader_redis.get("recent").to_json
+  else
+    {err: true}.to_json
+  end
 end
 
-get '/api/v1/hashtags/:term' do
-  Tweet.full_text_search(params[:label]).desc(:date_posted).limit(50).to_json
+get '/api/v1/:apitoken/hashtags/:term' do
+  if authorized? params[:apitoken]
+    Tweet.full_text_search(params[:label]).desc(:date_posted).limit(50).to_json
+  else
+    {err: true}.to_json
+  end
 end
 
 get '/api/v1/searches/:term' do
-  # byebug
-  Tweet.full_text_search(params[:word]).desc(:date_posted).limit(50).to_json
+  if authorized? params[:apitoken]
+    Tweet.full_text_search(params[:word]).desc(:date_posted).limit(50).to_json
+  else
+    {err: true}.to_json
+  end
 end
